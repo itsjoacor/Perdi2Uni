6f66a9e0804f0eba57cfe862583a57f854d480e8
Guido Gustavo Greco <guidogreco25@gmail.com>
2025-05-18 17:06:35 -0300

    Se realiza SCRUM-129

--- vite-project/src/components/NavbarAdmin.jsx
+++ vite-project/src/components/NavbarAdmin.jsx
@@ -22,10 +22,28 @@ const NavbarAdmin = () => {
   };
 
   return (
-	<nav className="bg-purple-700 text-white px-6 py-4 shadow-md flex justify-between items-center w-full">
-	  <Link to="/home">
-		<h1 className="text-xl font-bold whitespace-nowrap">Perdi2EnLaUni</h1>
-	  </Link>
+	<nav className="relative bg-purple-700 text-white px-6 py-4 shadow-md flex justify-between items-center w-full">
+	  {/* Logo a la izquierda */}
+    <Link to="/home" className="flex items-center">
+			<svg
+			  xmlns="http://www.w3.org/2000/svg"
+			  fill="none"
+			  viewBox="0 0 24 24"
+			  strokeWidth="1.5"
+			  stroke="currentColor"
+			  style={{ width: "37px", height: "37px" }}
+			>
+			  <path
+				strokeLinecap="round"
+				strokeLinejoin="round"
+				d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
+			  />
+			</svg>
+		  </Link>
+	
+	<nav className="absolute left-1/2 transform -translate-x-1/2">
+        <h1 className="text-2xl font-bold whitespace-nowrap">Perdi2EnLaUni</h1>
+      </nav>
 
 	  <div className="flex items-center divide-x divide-white">
 	  <Link to="/publicar" className="px-4 hover:underline whitespace-nowrap">
